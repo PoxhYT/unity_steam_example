e6709557a95d10e43b3682ac751919cb7a2dfbde
Michael Ernst <72230632+PoxhYT@users.noreply.github.com>
2022-10-02 20:49:26 +0200

    Added basic lobby design

--- Assets/Scripts/SteamLobby.cs
+++ Assets/Scripts/SteamLobby.cs
@@ -5,6 +5,8 @@ using TMPro;
 
 public class SteamLobby : MonoBehaviour
 {
+    public static SteamLobby Instance;
+
     protected Callback<LobbyCreated_t> LobbyCreated;
     protected Callback<GameLobbyJoinRequested_t> JoinRequest;
     protected Callback<LobbyEnter_t> LobbyEntered;
@@ -13,12 +15,10 @@ public class SteamLobby : MonoBehaviour
     private const string HostAddressKey = "HostAddress";
     CustomNetworkManager Manager;
 
-    [SerializeField] GameObject HostButton;
-    [SerializeField] TMP_Text LobbyName;
-
     private void Start()
     {
         if (!SteamManager.Initialized) { Debug.Log("Steam is not open!"); return; }
+        if(Instance == null) { Instance = this; }
         Manager = GetComponent<CustomNetworkManager>();
 
         LobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
@@ -48,10 +48,9 @@ public class SteamLobby : MonoBehaviour
 
     void OnLobbyEntered(LobbyEnter_t callback)
     {
-        HostButton.SetActive(false);
         CurrentLobbyID = callback.m_ulSteamIDLobby;
-        LobbyName.text = SteamMatchmaking.GetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), "name");
-        LobbyName.gameObject.SetActive(true);
+        /*LobbyName.text = SteamMatchmaking.GetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), "name");
+        LobbyName.gameObject.SetActive(true);*/
 
         if (NetworkServer.active) { return; }
         Manager.networkAddress = SteamMatchmaking.GetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), HostAddressKey);
