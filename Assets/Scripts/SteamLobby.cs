0c8bc569361ca5cea4ce432f687b653e040bfc3b
Michael Ernst <72230632+PoxhYT@users.noreply.github.com>
2022-10-02 20:23:26 +0200

    Initial commit

---
+++ Assets/Scripts/SteamLobby.cs
@@ -0,0 +1,60 @@
+using Mirror;
+using Steamworks;
+using UnityEngine;
+using TMPro;
+
+public class SteamLobby : MonoBehaviour
+{
+    protected Callback<LobbyCreated_t> LobbyCreated;
+    protected Callback<GameLobbyJoinRequested_t> JoinRequest;
+    protected Callback<LobbyEnter_t> LobbyEntered;
+
+    public ulong CurrentLobbyID;
+    private const string HostAddressKey = "HostAddress";
+    CustomNetworkManager Manager;
+
+    [SerializeField] GameObject HostButton;
+    [SerializeField] TMP_Text LobbyName;
+
+    private void Start()
+    {
+        if (!SteamManager.Initialized) { Debug.Log("Steam is not open!"); return; }
+        Manager = GetComponent<CustomNetworkManager>();
+
+        LobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
+        JoinRequest = Callback<GameLobbyJoinRequested_t>.Create(OnJoinReuest);
+        LobbyEntered = Callback<LobbyEnter_t>.Create(OnLobbyEntered);
+    }
+
+    public void HostLobby()
+    {
+        SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, Manager.maxConnections);
+    }
+
+    void OnLobbyCreated(LobbyCreated_t callback)
+    {
+        if (callback.m_eResult != EResult.k_EResultOK) { return; }
+        Debug.Log("Lobby created succesfully");
+        Manager.StartHost();
+        SteamMatchmaking.SetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), HostAddressKey, SteamUser.GetSteamID().ToString());
+        SteamMatchmaking.SetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), "name", SteamFriends.GetPersonaName().ToString() + "'S Lobby");
+    }
+
+    void OnJoinReuest(GameLobbyJoinRequested_t callback)
+    {
+        Debug.Log("Request to join");
+        SteamMatchmaking.JoinLobby(callback.m_steamIDLobby);
+    }
+
+    void OnLobbyEntered(LobbyEnter_t callback)
+    {
+        HostButton.SetActive(false);
+        CurrentLobbyID = callback.m_ulSteamIDLobby;
+        LobbyName.text = SteamMatchmaking.GetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), "name");
+        LobbyName.gameObject.SetActive(true);
+
+        if (NetworkServer.active) { return; }
+        Manager.networkAddress = SteamMatchmaking.GetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), HostAddressKey);
+        Manager.StartClient();
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# unity_steam_example
+ This is a Unity Steam example feel free to use this project to test everything arround the steam api
